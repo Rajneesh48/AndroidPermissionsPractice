ff507cdaaf621603bc7455452f0482fa07deb6a3
Rajneesh Kumar <goldberg9087@gmail.com>
2026-05-07 13:12:31 +0530

    just new changes

--- app/src/main/java/com/example/androidpermissionspractice/PermissionsPractice.kt
+++ app/src/main/java/com/example/androidpermissionspractice/PermissionsPractice.kt
@@ -16,7 +16,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.ElevatedButton
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
